220890bd703d1ddc3eb4af97235e980125bf6186
reyanne2 <reyanne2606@.com>
2024-05-17 12:01:43 -0300

    modification des exercices 11,12

--- 10-execice/js/nombre.js
+++ 10-execice/js/nombre.js
@@ -3,9 +3,9 @@ var fact = 1;
 
 function facto(){
 		for (var i = nb; i>1; i-= 1)
-	{
+	
 		fact *= i;
 		alert(" Factorielle " + nb + " = " + fact );
-	}
+	
 }
 
--- 12-Exercice/js/for.js
+++ 12-Exercice/js/for.js
@@ -2,10 +2,10 @@ function boucle(){
 		for ( var i = 0; i < 5; i++ )
 	{
 		if( i == 3)
-		{
+		
 			break;
 			document.write("<br> i est: " + i);
-		}
+		
 	}
 	document.write("<br> Apres la boucle");
 }
