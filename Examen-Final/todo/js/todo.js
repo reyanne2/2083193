3ceebeca81771e3e83fa5043030e1fc2f4cea709
reyanne2 <reyanne2606@.com>
2024-06-06 09:25:57 -0300

    second commit

---
+++ Examen-Final/todo/js/todo.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(event) {
+    var ajoutTask = document.getElementById('ajoutTask');
+    var boutonAjout = document.getElementById('boutonAjout');
+    var supprimer = document.getElementById('supprimer');
+    var Liste = document.getElementById('Liste');
+    var TotalTask = document.getElementById('TotalTask');
+});
+
+
+    
